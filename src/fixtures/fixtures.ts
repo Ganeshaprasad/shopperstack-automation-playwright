5061dafd0f407a09587aeac24cdf3ff45ce4a524
Ganeshaprasad <ganesha.prasad1996@gmail.com>
2025-12-17 19:38:01 +0530

    created playwright project

---
+++ src/fixtures/fixtures.ts
@@ -0,0 +1,5 @@
+import { test, expect } from '@playwright/test';
+//import { LoginPage } from '../../pages/LoginPage';
+
+test('Login', async ({ page }) => {
+  //const loginPage = new LoginPage(page);  // Manual creation
